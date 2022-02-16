d69c2aa71a33a7dbc1613b49fc0e1eb63b45c5f9
Арапат <fresco2209@gmail.com>
2022-02-16 04:43:11 +0300

    add secret_key to action secrets

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', default=False)
+# DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
