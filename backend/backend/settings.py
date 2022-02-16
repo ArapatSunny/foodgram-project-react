b6726bf1616b93d4fd9e214c4bd58fee80135acc
Арапат <fresco2209@gmail.com>
2022-02-16 04:05:55 +0300

    change key name of db from DB_NAME to POSTGRES_DB

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,9 +62,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME", default="postgres"),
-        "USER": os.getenv("POSTGRES_USER", default="postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postges"),
+        "NAME": os.getenv("POSTGRES_DB", default="foodgram"),
+        "USER": os.getenv("POSTGRES_USER", default="fooduser"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="food1501"),
         "HOST": os.getenv("DB_HOST", default="host"),
         "PORT": os.getenv("DB_PORT", default="1111"),
     }
