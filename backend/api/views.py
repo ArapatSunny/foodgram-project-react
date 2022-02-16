292089c34fe670e2b549902d10b5964962dcc5fc
Арапат <fresco2209@gmail.com>
2022-02-16 15:58:24 +0300

    comment permission classes for tags and ingredients

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import filters, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from foodgram.models import (Ingredient, IngredientInRecipe, Recipe,
