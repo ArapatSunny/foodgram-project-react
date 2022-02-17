4e371f297cf560056d350c3a296e70470d3fa532
Арапат <fresco2209@gmail.com>
2022-02-17 19:02:22 +0300

    change permission access

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthenticatedOwnerOrAdminOnly(permissions.BasePermission):
-    message = "Access only for owner or admin!"
+    message = "Access to edit only for owner or admin!"
 
     def has_permission(self, request, view):
         return (
@@ -13,5 +13,17 @@ class IsAuthenticatedOwnerOrAdminOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or obj.author == request.user.is_authenticated
+            or obj.author == request.user
             or request.user.is_superuser)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    message = "Access only for admin!"
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_superuser)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_superuser)
